a40ed3a6d71c281b6c152d36ec642a2037b2e99f
Thom Neale <twneale@gmail.com>
2012-12-13 20:44:58 -0500

    changes as of 2012-12-13T20:44:58.681456

--- update.py
+++ update.py
@@ -1,8 +1,9 @@
+import datetime
 from subprocess import check_call
 import scrape
 
 scrape.main()
 cmd = 'git add .'
 check_call(cmd, shell=True)
-cmd = 'git commit -am"changes as of 2012-12-13T19:21:41.723576"'
+cmd = 'git commit -am"changes as of %s"' % datetime.datetime.now().isoformat()
 check_call(cmd, shell=True)
