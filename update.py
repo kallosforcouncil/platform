d1e76290f45577177fb38bd7f8876e7aeb525b0e
OpenLegislation <ben@kallosforcouncil.com>
2012-12-13 21:29:54 -0500

    spruce update script

--- update.py
+++ update.py
@@ -6,12 +6,24 @@ import scrape
 
 
 def main():
+
+    # Update.
     scrape.main()
+
+    # Add any new files.
     cmd = 'git add .'
     check_call(cmd, shell=True)
+
+    # Commit changes.
     cmd = 'git commit -am"changes as of %s"'
     cmd = cmd % datetime.datetime.now().isoformat()
     check_call(cmd, shell=True)
+
+    # Pull changes.
+    cmd = 'git pull origin master'
+    check_call(cmd, shell=True) 
+
+    # Push changes.   
     cmd = 'git push origin master'
     check_call(cmd, shell=True)
 
