73aee1ed8c3883903864c221f09b50750ec62dd1
twneale <ben@kallosforcouncil.com>
2013-08-28 21:54:58 -0400

    needs to push

--- update.py
+++ update.py
@@ -2,8 +2,20 @@ import datetime
 from subprocess import check_call
 import scrape
 
-scrape.main()
-cmd = 'git add .'
-check_call(cmd, shell=True)
-cmd = 'git commit -am"changes as of %s"' % datetime.datetime.now().isoformat()
-check_call(cmd, shell=True)
+
+def main():
+    scrape.main()
+
+    cmd = 'git add .'
+    check_call(cmd, shell=True)
+
+    cmd = 'git commit -am"changes as of %s"'
+    cmd = cmd % datetime.datetime.now().isoformat()
+    check_call(cmd, shell=True)
+
+    cmd = 'git push origin master'
+    check_call(cmd, shell=True)
+
+
+if __name__ == '__main__':
+    main()
